191037abfb2ffdb05b30374c19f41da2e8abbdcb
christophercsmall <christopher.c.small@gmail.com>
2016-10-11 08:20:25 -0400

    resized

--- PhoneSystemInventoryManager/PhoneSystemInventoryManager/Form1.Designer.cs
+++ PhoneSystemInventoryManager/PhoneSystemInventoryManager/Form1.Designer.cs
@@ -38,11 +38,12 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.ciscoPhoneSystemDBDataSet = new PhoneSystemInventoryManager.CiscoPhoneSystemDBDataSet();
-            this.ciscoPhoneSystemDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.query2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ciscoPhoneSystemDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ciscoPhoneSystemDBDataSet = new PhoneSystemInventoryManager.CiscoPhoneSystemDBDataSet();
             this.query2TableAdapter = new PhoneSystemInventoryManager.CiscoPhoneSystemDBDataSetTableAdapters.Query2TableAdapter();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.fNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.lNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.companyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -54,11 +55,14 @@
             this.switchPortNumDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.patchPanelNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.patchPanelPortNumDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.query2BindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ciscoPhoneSystemDBDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ciscoPhoneSystemDBDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.query2BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ciscoPhoneSystemDBDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ciscoPhoneSystemDBDataSet)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.query2BindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -68,7 +72,7 @@
             this.optionsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(849, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1186, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -85,13 +89,13 @@
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
             this.fileToolStripMenuItem.Text = "Connect...";
             // 
             // closeToolStripMenuItem
             // 
             this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
             this.closeToolStripMenuItem.Text = "Close";
             // 
             // optionsToolStripMenuItem
@@ -105,7 +109,7 @@
             // settingsToolStripMenuItem
             // 
             this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
             this.settingsToolStripMenuItem.Text = "Settings";
             // 
             // comboBox1
@@ -123,30 +127,59 @@
             "IDF Name",
             "Switch Name (DNS)",
             "Patch Panel Name"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 39);
+            this.comboBox1.Location = new System.Drawing.Point(12, 27);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(162, 21);
+            this.comboBox1.Size = new System.Drawing.Size(408, 21);
             this.comboBox1.TabIndex = 3;
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(348, 39);
+            this.button1.Location = new System.Drawing.Point(161, 54);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.Size = new System.Drawing.Size(319, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(180, 42);
+            this.textBox1.Location = new System.Drawing.Point(12, 54);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
             this.textBox1.TabIndex = 5;
+            // 
+            // query2BindingSource
+            // 
+            this.query2BindingSource.DataMember = "Query2";
+            this.query2BindingSource.DataSource = this.ciscoPhoneSystemDBDataSetBindingSource;
+            // 
+            // ciscoPhoneSystemDBDataSetBindingSource
+            // 
+            this.ciscoPhoneSystemDBDataSetBindingSource.DataSource = this.ciscoPhoneSystemDBDataSet;
+            this.ciscoPhoneSystemDBDataSetBindingSource.Position = 0;
+            // 
+            // ciscoPhoneSystemDBDataSet
+            // 
+            this.ciscoPhoneSystemDBDataSet.DataSetName = "CiscoPhoneSystemDBDataSet";
+            this.ciscoPhoneSystemDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // query2TableAdapter
+            // 
+            this.query2TableAdapter.ClearBeforeFill = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(12, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1168, 338);
+            this.panel1.TabIndex = 6;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -162,103 +195,102 @@
             this.switchPortNumDataGridViewTextBoxColumn,
             this.patchPanelNameDataGridViewTextBoxColumn,
             this.patchPanelPortNumDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.query2BindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 68);
+            this.dataGridView1.DataSource = this.query2BindingSource1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(825, 339);
-            this.dataGridView1.TabIndex = 6;
-            // 
-            // ciscoPhoneSystemDBDataSet
-            // 
-            this.ciscoPhoneSystemDBDataSet.DataSetName = "CiscoPhoneSystemDBDataSet";
-            this.ciscoPhoneSystemDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // ciscoPhoneSystemDBDataSetBindingSource
-            // 
-            this.ciscoPhoneSystemDBDataSetBindingSource.DataSource = this.ciscoPhoneSystemDBDataSet;
-            this.ciscoPhoneSystemDBDataSetBindingSource.Position = 0;
-            // 
-            // query2BindingSource
-            // 
-            this.query2BindingSource.DataMember = "Query2";
-            this.query2BindingSource.DataSource = this.ciscoPhoneSystemDBDataSetBindingSource;
-            // 
-            // query2TableAdapter
-            // 
-            this.query2TableAdapter.ClearBeforeFill = true;
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(1168, 338);
+            this.dataGridView1.TabIndex = 0;
             // 
             // fNameDataGridViewTextBoxColumn
             // 
             this.fNameDataGridViewTextBoxColumn.DataPropertyName = "FName";
             this.fNameDataGridViewTextBoxColumn.HeaderText = "FName";
             this.fNameDataGridViewTextBoxColumn.Name = "fNameDataGridViewTextBoxColumn";
+            this.fNameDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // lNameDataGridViewTextBoxColumn
             // 
             this.lNameDataGridViewTextBoxColumn.DataPropertyName = "LName";
             this.lNameDataGridViewTextBoxColumn.HeaderText = "LName";
             this.lNameDataGridViewTextBoxColumn.Name = "lNameDataGridViewTextBoxColumn";
+            this.lNameDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // companyDataGridViewTextBoxColumn
             // 
             this.companyDataGridViewTextBoxColumn.DataPropertyName = "Company";
             this.companyDataGridViewTextBoxColumn.HeaderText = "Company";
             this.companyDataGridViewTextBoxColumn.Name = "companyDataGridViewTextBoxColumn";
+            this.companyDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // extensionNumDataGridViewTextBoxColumn
             // 
             this.extensionNumDataGridViewTextBoxColumn.DataPropertyName = "ExtensionNum";
             this.extensionNumDataGridViewTextBoxColumn.HeaderText = "ExtensionNum";
             this.extensionNumDataGridViewTextBoxColumn.Name = "extensionNumDataGridViewTextBoxColumn";
+            this.extensionNumDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // mACDataGridViewTextBoxColumn
             // 
             this.mACDataGridViewTextBoxColumn.DataPropertyName = "MAC";
             this.mACDataGridViewTextBoxColumn.HeaderText = "MAC";
             this.mACDataGridViewTextBoxColumn.Name = "mACDataGridViewTextBoxColumn";
+            this.mACDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // typeDataGridViewTextBoxColumn
             // 
             this.typeDataGridViewTextBoxColumn.DataPropertyName = "Type";
             this.typeDataGridViewTextBoxColumn.HeaderText = "Type";
             this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
+            this.typeDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // iDFNameDataGridViewTextBoxColumn
             // 
             this.iDFNameDataGridViewTextBoxColumn.DataPropertyName = "IDFName";
             this.iDFNameDataGridViewTextBoxColumn.HeaderText = "IDFName";
             this.iDFNameDataGridViewTextBoxColumn.Name = "iDFNameDataGridViewTextBoxColumn";
+            this.iDFNameDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // dNSNameDataGridViewTextBoxColumn
             // 
             this.dNSNameDataGridViewTextBoxColumn.DataPropertyName = "DNSName";
             this.dNSNameDataGridViewTextBoxColumn.HeaderText = "DNSName";
             this.dNSNameDataGridViewTextBoxColumn.Name = "dNSNameDataGridViewTextBoxColumn";
+            this.dNSNameDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // switchPortNumDataGridViewTextBoxColumn
             // 
             this.switchPortNumDataGridViewTextBoxColumn.DataPropertyName = "SwitchPortNum";
             this.switchPortNumDataGridViewTextBoxColumn.HeaderText = "SwitchPortNum";
             this.switchPortNumDataGridViewTextBoxColumn.Name = "switchPortNumDataGridViewTextBoxColumn";
+            this.switchPortNumDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // patchPanelNameDataGridViewTextBoxColumn
             // 
             this.patchPanelNameDataGridViewTextBoxColumn.DataPropertyName = "PatchPanelName";
             this.patchPanelNameDataGridViewTextBoxColumn.HeaderText = "PatchPanelName";
             this.patchPanelNameDataGridViewTextBoxColumn.Name = "patchPanelNameDataGridViewTextBoxColumn";
+            this.patchPanelNameDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // patchPanelPortNumDataGridViewTextBoxColumn
             // 
             this.patchPanelPortNumDataGridViewTextBoxColumn.DataPropertyName = "PatchPanelPortNum";
             this.patchPanelPortNumDataGridViewTextBoxColumn.HeaderText = "PatchPanelPortNum";
             this.patchPanelPortNumDataGridViewTextBoxColumn.Name = "patchPanelPortNumDataGridViewTextBoxColumn";
+            this.patchPanelPortNumDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // query2BindingSource1
+            // 
+            this.query2BindingSource1.DataMember = "Query2";
+            this.query2BindingSource1.DataSource = this.ciscoPhoneSystemDBDataSetBindingSource;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 419);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1186, 433);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
@@ -269,10 +301,12 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ciscoPhoneSystemDBDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ciscoPhoneSystemDBDataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.query2BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ciscoPhoneSystemDBDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ciscoPhoneSystemDBDataSet)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.query2BindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -288,11 +322,12 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.BindingSource ciscoPhoneSystemDBDataSetBindingSource;
         private CiscoPhoneSystemDBDataSet ciscoPhoneSystemDBDataSet;
         private System.Windows.Forms.BindingSource query2BindingSource;
         private CiscoPhoneSystemDBDataSetTableAdapters.Query2TableAdapter query2TableAdapter;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn fNameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn lNameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn companyDataGridViewTextBoxColumn;
@@ -304,6 +339,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn switchPortNumDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn patchPanelNameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn patchPanelPortNumDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource query2BindingSource1;
     }
 }
 
